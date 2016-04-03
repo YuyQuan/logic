29d9864d1b72af18618df77439ffbc8895a3de5d
Quan <yungquinnman@gmail.com>
2016-04-02 23:16:15 -0500

    powershell

---
+++ powershell/logic.ps1
@@ -0,0 +1,22 @@
+﻿
+
+function n {
+	Write-Host 'is'
+	h
+}
+function l {
+	Write-Host 'this'
+	y
+}
+function h {
+	Write-Host 'broken'
+}
+function y {
+	Write-Host 'logic'
+	n
+}
+l
+Write-Host 'this'
+Write-Host 'logic'
+Write-Host 'is'
+Write-Host 'broken'
